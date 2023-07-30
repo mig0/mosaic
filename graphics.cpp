29168ba6abed49b59dad9f3897ac9e7e4e0d6f57
Mikhael Goikhman <migo@freeshell.org>
2023-07-30 11:47:22 +0300

    Add individual hover and active colors for colored grid buttons
    Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

--- graphics.cpp
+++ graphics.cpp
@@ -13,12 +13,22 @@ MosaicWindow::MosaicWindow(Grid &grid0) : grid(grid0) {
 		button.red    { background-image: image(rgb(180, 30, 30));   border-color: rgb(80, 10, 10); } \
 		button.white  { background-image: image(rgb(240, 240, 240)); border-color: rgb(120, 120, 120); } \
 		button.yellow { background-image: image(rgb(240, 210, 30));  border-color: rgb(120, 100, 10); } \
-		button.orange { background-image: image(rgb(240, 100, 30));  border-color: rgb(120, 50, 10);  } \
+		button.orange { background-image: image(rgb(240, 140, 30));  border-color: rgb(120, 70, 10);  } \
 		button.blue   { background-image: image(rgb(30, 30, 180));   border-color: rgb(10, 10, 90); } \
 		button.green  { background-image: image(rgb(30, 180, 30));   border-color: rgb(10, 100, 10); } \
+		button.red:hover    { background-image: image(rgb(220, 48, 48)); } \
+		button.white:hover  { background-image: image(rgb(255, 190, 190)); } \
+		button.yellow:hover { background-image: image(rgb(255, 240, 48)); } \
+		button.orange:hover { background-image: image(rgb(255, 170, 48)); } \
+		button.blue:hover   { background-image: image(rgb(48, 48, 220)); } \
+		button.green:hover  { background-image: image(rgb(48, 220, 48)); } \
+		button.red:active    { background-image: image(rgb(140, 18, 18)); } \
+		button.white:active  { background-image: image(rgb(190, 190, 190)); } \
+		button.yellow:active { background-image: image(rgb(190, 170, 18)); } \
+		button.orange:active { background-image: image(rgb(190, 100, 18)); } \
+		button.blue:active   { background-image: image(rgb(18, 18, 140)); } \
+		button.green:active  { background-image: image(rgb(18, 140, 18)); } \
 		button { border: solid 1px; font: 8px \"Sans\"; margin: 0px; padding: 0px; min-width: 20px; min-height: 20px; } \
-		button:hover { background-image: image(pink); } \
-		button:active { background-image: image(purple); } \
 	");
 
 	set_child(main_box);
